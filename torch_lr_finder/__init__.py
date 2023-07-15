53ba89f43b36a3d23fdd997fce8b28bf80856405
garima-mahato <m.garima318@gmail.com>
2023-07-15 11:32:10 +0530

    Update __init__.py

--- torch_lr_finder/__init__.py
+++ torch_lr_finder/__init__.py
@@ -1,3 +1,4 @@
-from torch_lr_finder.lr_finder import LRFinder
-from torch_lr_finder.lr_finder import TrainDataLoaderIter
-from torch_lr_finder.lr_finder import ValDataLoaderIter
+# from torch_lr_finder.lr_finder import LRFinder
+# from torch_lr_finder.lr_finder import TrainDataLoaderIter
+# from torch_lr_finder.lr_finder import ValDataLoaderIter
+from .lr_finder import *
